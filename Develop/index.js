11bcbf550baf5e02418ed7eed487d50d079d9772
Teojin02 <Teojincelestino@gmail.com>
2020-10-03 17:12:27 -0700

    commit on master

--- Develop/index.js
+++ Develop/index.js
@@ -1,86 +1,113 @@
-//npm package dependecy 
+const inquirer = require("inquirer");
 const fs = require("fs");
-
-
-//data files for generation of readme
-const userPrompts = require("./utils/userPrompts");
-const generateMarkdown = require ("./utils/generateMarkdown");
-
-
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-    {
-        type: "input",
-        message: "What is your GitHub user name?",
-        name: "userName",
-      },
-      {
-        type: "input",
-        message: "Enter the name of your GitHub repository for this project?",
-        name: "repo",
-      },
-      {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title",
-      },
-      {
-        type: "input",
-        message: "What is the description of your project?",
-        name: "description",
-      },
-      {
-        type: "input",
-        message: "What are the installation instructions for your project?",
-        name: "install",
-      },
-      {
-        type: "input",
-        message: "Please provide instructions and examples for use.",
-        name: "usage",
-      },
-      {
-        type: "list",
-        message:
-          "Please choose the type of license you would like for your project:",
-        choices: ["MIT", "Apache", "GPLv2"],
-        name: "userLicense",
-      },
-      {
-        type: "input",
-        message:
-          "Please provide guidelines for how other developers can contribute to your project.",
-        name: "contributing",
-      },
-      {
-        type: "input",
-        message:
-          "Please provide examples of tests to run in your application, and instructions on how to run them.",
-        name: "tests",
-      },
-      {
-        type: "input",
-        message:
-          "Please provide your email address to be contacted about questions regarding your app.",
-        name: "email",
-      },
+const questions = [{
+    type: "input",
+    message: "Please provide a GITHUB Username",
+    name: "UserName"
+  },
+  {
+    type: "input",
+    message: "Please provide you eamil address to be contacted about questions regarding your app.",
+    name: "Useremail"
+  },
+  {
+    type: "input",
+    message: "Please provide the name of your project",
+    name: "Userproject"
+  },
+  {
+    type: "input",
+    message: "Please enter the name of your GitHub respository for this project?",
+    name: "Userrepo"
+  },
+  {
+    type: "input",
+    message: "Please provide description of your project",
+    name: "UserDescription"
+  },
+  {
+    type: "list",
+    message: "What kind of license should your project have?",
+    name: "Licence",
+    choices: [
+        "MIT", 
+        "GPLv3", 
+        "Apache",
+        "BSD 3"
+      ] 
+  },
+  {
+    type: "input",
+    message: "What command should be run to install dependencies?",
+    name: "InstallDep"
+  },
+  {
+    type: "input",
+    message: "What command should be run to run tests?",
+    name: "UserTest"
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about contributing to the repo?",
+    name: "Contribute"
+  }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName + "date", ".md" , function (err){
+    fs.writeFile(fileName, data, err => {
         if (err) {
+          throw err;
         }
-        console.log ("Done");
-    });
+        console.log("Successful");
+      });
 }
 
 // function to initialize program
 function init() {
-let data =  userPrompts();
-let md = generateMarkdown (data);
-writeToFile ("README-Test", md);
+    inquirer
+    .prompt(questions)
+    .then(function({
+      UserName, 
+      Useremail, 
+      Userproject,
+      Userrepo, 
+      UserDescription, 
+      Licence, 
+      InstallDep, 
+      UserTest, 
+      Contribute}) {
+        if (
+        UserName !== "" && 
+        Useremail !== "" && 
+        Userproject !=="" &&
+        Userrepo !=="" && 
+        UserDescription !=="" && 
+        Licence !=="" && 
+        InstallDep !=="" && 
+        UserTest !=="" &&
+        Contribute !=="") {
+         ;
+
+            writeToFile("README.md", generateMarkdown({
+              UserName, 
+              Useremail, 
+              Userproject,
+              Userrepo, 
+              UserDescription, 
+              Licence, 
+              InstallDep, 
+              UserTest, 
+              Contribute}));
+        }
+        else {
+            console.log("Please enter all information");
+            init();
+        }
+    })
 }
 
 // function call to initialize program
